2ed32743f8ad1180d2ae150c286340a0cc98d4c4
Tom Reed <t.reed1104@gmail.com>
2020-09-24 14:35:13 +0100

    Added the Consumables Weakaura group

---
+++ Consumables.lua
@@ -0,0 +1,113 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "Missing: Agility",
+        [2] = "Missing: Strength",
+        [3] = "Missing: Firewater",
+        [4] = "Missing: Food",
+        [5] = "Missing: Spirit of Zanza",
+        [6] = "Missing: Fortitude",
+        [7] = "Protection: Nature",
+        [8] = "Protection: Arcane",
+        [9] = "Protection: Shadow",
+        [10] = "Protection: Fire",
+        [11] = "Active: LIP",
+        [12] = "Active: FAP",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["authorOptions"] = {
+    },
+    ["xOffset"] = 0,
+    ["border"] = false,
+    ["yOffset"] = -376,
+    ["anchorPoint"] = "CENTER",
+    ["borderSize"] = 2,
+    ["scale"] = 1,
+    ["borderColor"] = {
+        [1] = 0,
+        [2] = 0,
+        [3] = 0,
+        [4] = 1,
+    },
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["names"] = {
+                },
+                ["type"] = "aura2",
+                ["spellIds"] = {
+                },
+                ["subeventSuffix"] = "_CAST_START",
+                ["unit"] = "player",
+                ["subeventPrefix"] = "SPELL",
+                ["event"] = "Health",
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["borderEdge"] = "Square Full White",
+    ["internalVersion"] = 33,
+    ["regionType"] = "group",
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["id"] = "Consumables",
+    ["borderInset"] = 1,
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+    },
+    ["uid"] = "zRThrFxVSyQ",
+    ["borderOffset"] = 4,
+    ["subRegions"] = {
+    },
+    ["conditions"] = {
+    },
+    ["load"] = {
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["config"] = {
+    },
+}
